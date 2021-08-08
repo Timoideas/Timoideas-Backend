58ffbd7ea306c098b35977751c5d4a42ef2db942
Timoideas <Timoideas@hotmail.com>
2021-08-08 18:32:36 -0500

    Origin

--- src/sockets/index.socket.js
+++ src/sockets/index.socket.js
@@ -3,7 +3,7 @@ import { server } from '../server';
 export const socket = new Server(server, {
   // path: '/timoideas',
   cors: {
-    origin: false,
+    origin: true,
     methods: ['GET', 'POST'],
     credentials: true,
     allowedHeaders: ['hola'],
